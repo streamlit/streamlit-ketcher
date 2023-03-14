6c56f8efcde5b298155474f9f981c4ab109f33bc
Kamil Bregula <kamil.bregula@snowflake.com>
2023-03-14 13:45:21 +0100

    Clean up __init__.py

--- my_component/__init__.py
+++ my_component/__init__.py
@@ -1,49 +1,21 @@
-import os
+from pathlib import Path
 import streamlit.components.v1 as components
 
 # Create a _RELEASE constant. We'll set this to False while we're developing
 # the component, and True when we're ready to package and distribute it.
-# (This is, of course, optional - there are innumerable ways to manage your
-# release process.)
 _RELEASE = False
 
-# Declare a Streamlit component. `declare_component` returns a function
-# that is used to create instances of the component. We're naming this
-# function "_component_func", with an underscore prefix, because we don't want
-# to expose it directly to users. Instead, we will create a custom wrapper
-# function, below, that will serve as our component's public API.
-
-# It's worth noting that this call to `declare_component` is the
-# *only thing* you need to do to create the binding between Streamlit and
-# your component frontend. Everything else we do in this file is simply a
-# best practice.
-
 if not _RELEASE:
-    _component_func = components.declare_component(
-        # We give the component a simple, descriptive name ("my_component"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
-        "my_component",
-        # Pass `url` here to tell Streamlit that the component will be served
-        # by the local dev server that you run via `npm run start`.
-        # (This is useful while your component is in development.)
+    _render_component = components.declare_component(
+        "streamlit_ketcher",
         url="http://localhost:3000",
     )
 else:
-    # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to to the component's
-    # build directory:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component("my_component", path=build_dir)
+    build_dir = Path(__file__).parent / "frontend" / "build"
+    _render_component = components.declare_component("streamlit_ketcher", path=build_dir)
 
 
-# Create a wrapper function for the component. This is an optional
-# best practice - we could simply expose the component function returned by
-# `declare_component` and call it done. The wrapper allows us to customize
-# our component's API: we can pre-process its input args, post-process its
-# output value, and add a docstring for users.
-def ketcher(molecule, key=None):
+def st_ketcher(molecule, key=None):
     """Create a new instance of "my_component".
 
     Parameters
@@ -63,29 +35,4 @@ def ketcher(molecule, key=None):
         frontend.)
 
     """
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    #
-    # "default" is a special argument that specifies the initial return
-    # value of the component before the user has interacted with it.
-    component_value = _component_func(molecule=molecule, key=key, default=None)
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
-    return component_value
-
-
-# Add some test code to play with the component while it's in development.
-# During development, we can run this just as we would any other Streamlit
-# app: `$ streamlit run my_component/__init__.py`
-if not _RELEASE:
-    import streamlit as st
-
-    st.set_page_config(layout="wide")
-    st.subheader("Component with constant args")
-
-    molecule = st.text_input("Molecule", "CCO")
-    smile_code = ketcher(molecule)
-    st.markdown("Smile code: %s" % smile_code)
-
+    return _render_component(molecule=molecule, key=key, default=molecule)
---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.subheader("Component with constant args")
+
+molecule = st.text_input("Molecule", "CCO")
+smile_code = st_ketcher(molecule)
+st.markdown("Smile code: %s" % smile_code)
