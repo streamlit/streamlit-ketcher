79935b9680ade1c19e60a5343eda0d46a8525a10
Kamil Bregula <kamil.bregula@snowflake.com>
2023-03-17 07:41:40 +0100

    dev.py manages venv

--- dev.py
+++ dev.py
@@ -6,6 +6,8 @@ import subprocess
 from pathlib import Path
 
 THIS_DIRECTORY = Path(__file__).parent
+VENV_DIRECTORY = THIS_DIRECTORY / "venv"
+PYTHON_BIN = VENV_DIRECTORY / "bin" / "python"
 
 
 def run_verbose(cmd_args, *args, **kwargs):
@@ -15,9 +17,42 @@ def run_verbose(cmd_args, *args, **kwargs):
     subprocess.run(cmd_args, *args, **kwargs)
 
 
+def ensure_environment():
+    try:
+        subprocess.check_call(['python', '-m', 'venv', '--help'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        raise SystemExit("'venv' python module is not installed")
+
+    if not PYTHON_BIN.exists():
+        shell_cmd = shlex.join([str(__file__), "py-create-venv"])
+        raise SystemExit(
+            "The virtual environment is not exists.\n"
+            "To create environment run:"
+            f"   $ {shell_cmd}")
+
+    try:
+        subprocess.check_call(['node', '--version'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        raise SystemExit("'node' is not installed")
+
+    try:
+        subprocess.check_call(['yarn', '--version'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        raise SystemExit("'yarn' is not installed")
+
+
+def cmd_py_create_venv(args):
+    run_verbose(
+        ["python", "-m", "venv", str(VENV_DIRECTORY)], cwd=THIS_DIRECTORY
+    )
+    run_verbose(
+        [str(PYTHON_BIN), "-m", "pip", "install", "-r", str(THIS_DIRECTORY / "dev-requirements.txt")],
+        cwd=THIS_DIRECTORY
+    )
+
+
 def cmd_py_distribute(args):
-    run_verbose(['pip', 'install', 'wheel'], cwd=THIS_DIRECTORY)
-    run_verbose(['python', 'setup.py', 'bdist_wheel', '--universal', 'sdist'], cwd=THIS_DIRECTORY)
+    run_verbose([str(PYTHON_BIN), 'setup.py', 'bdist_wheel', '--universal', 'sdist'], cwd=THIS_DIRECTORY)
 
 
 def cmd_js_build(args):
@@ -43,12 +78,19 @@ def get_parser():
     subparsers.add_parser(
         'package', help='Build frontend and then run "py-distribution".'
     ).set_defaults(func=cmd_package)
+    subparsers.add_parser(
+        'py-create-venv', help='Create virtual environment for Python.'
+    ).set_defaults(func=cmd_py_create_venv)
     return parser
 
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
+
+    print(args.subcommand)
+    if args.subcommand != 'py-create-venv':
+        ensure_environment()
     args.func(args)
 
 
