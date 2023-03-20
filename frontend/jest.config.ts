03d121b5bd53043d84c8fee580f6b74f92be8a2d
Kamil Bregula <kamil.bregula@snowflake.com>
2023-03-20 11:12:48 +0100

    fixup! fixup! Add unit tests for frontend

--- frontend/jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type {Config} from '@jest/types';
-
-export default async (): Promise<Config.InitialOptions> => {
-    return {
-        transformIgnorePatterns: ['/node_modules/(?!(streamlit-component-lib)/)', "\\.pnp\\.[^\\\/]+$"],
-        verbose: true,
-    };
-};
